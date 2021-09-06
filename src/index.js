6599cc9562b09c772ea3b6a5fe0eee8e096d4950
KarinaStabile <karina.stabile@gmail.com>
2021-09-05 22:39:35 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  //listen for submit button click, and event.preventDefault method
+  document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    handleToDo(e.target['new-task-description'].value);
+    // this is the part that needs help. How to target that text input
+  })
+})
+
+function handleToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  //this takes string, and builds that element on string it matches up with
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove(); 
+}
